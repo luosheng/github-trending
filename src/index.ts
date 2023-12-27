c639449cb5cf855612da65d8e53c8da9bfdcab75
Sheng Luo <luosheng1986@gmail.com>
2023-12-27 09:11:20 +0800

    Add /repositories endpoint to fetch trending repositories

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/repositories', async (c) => {
+  const response = await fetch("https://github.com/trending")
+  const html = await response.text() // HTML string
+  return c.text(html)
+})
 
 export default app
